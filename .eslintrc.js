3ea290ad184a463e8e0972d045f92b3a134c1f6a
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 02:48:45 +0900

    Refactor: 기존 function을 화살표 함수로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,10 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+    ],
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'import/extensions': ['error', 'ignorePackages'],
   },
