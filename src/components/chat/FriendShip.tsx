dbe865984a48dded23e53d3a71a2e5a1e095daea
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-15 02:09:10 +0900

    Refactor: 친밀도 컴포넌트 CSS 리팩토링

--- src/components/chat/FriendShip.tsx
+++ src/components/chat/FriendShip.tsx
@@ -9,59 +9,60 @@ interface UserStatusProps {
 
 const FriendShip:FC<UserStatusProps> = ({ friendShipExp, maxFriendShipExp, friendShipLv }) => (
   <div
-    css={css`
-      height: 23px;
-      text-align: left;
-      font-size: 10px;
-      color:  #797C7B;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    `}
+    css={FriendShipCSS}
   >
-    <div css={css`
-      display:flex;
-      align-items: center;
-    `}
-    >
+    <div css={friendShipTextAreaCSS}>
       <div css={css`padding-right: 5px;`}>친밀도</div>
-      <span
-        css={css`
-          font-size: 12px;
-          color: #20a090;
-        `}
-      >
+      <span css={css`font-size: 12px; color: #20a090;`}>
         Lv.
         {' '}
         {friendShipLv}
       </span>
     </div>
-    <div css={css`
-      display: flex;
-      padding:5px;
-    `}
-    >
+    <div css={expBarAreaCSS}>
       <div
-        css={css`
-          /* position: absolute; */
-          border-top: 4px solid #cdd1d0;
-          width: ${(1 - (friendShipExp / maxFriendShipExp)) * 60}px;
-          height: 4px;
-          opacity: 0.5;
-          border-radius: 5px;
-        `}
+        css={maxExpBarCSS(friendShipExp, maxFriendShipExp)}
       />
       <div
-        css={css`
-          /* position: absolute; */
-          border-top: 4px solid #20a090;
-          width: ${(friendShipExp / maxFriendShipExp) * 60}px;
-          height: 4px;
-          border-radius: 5px;
-        `}
+        css={expBarCSS(friendShipExp, maxFriendShipExp)}
       />
     </div>
   </div>
 );
 
 export default FriendShip;
+
+const FriendShipCSS = css`
+  height: 23px;
+  text-align: left;
+  font-size: 10px;
+  color:  #797C7B;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const friendShipTextAreaCSS = css`
+display:flex;
+align-items: center;
+`;
+
+const expBarAreaCSS = css`
+  display: flex;
+  padding:5px;
+`;
+
+const maxExpBarCSS = (friendShipExp : number, maxFriendShipExp : number) => css`
+border-top: 4px solid #cdd1d0;
+width: ${(1 - (friendShipExp / maxFriendShipExp)) * 60}px;
+height: 4px;
+opacity: 0.5;
+border-radius: 5px;
+`;
+
+const expBarCSS = (friendShipExp : number, maxFriendShipExp : number) => css`
+border-top: 4px solid #20a090;
+width: ${(friendShipExp / maxFriendShipExp) * 60}px;
+height: 4px;
+border-radius: 5px;
+`;
