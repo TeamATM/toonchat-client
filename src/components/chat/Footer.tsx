5cb97a67a2aad2f429ea0125bbd57cca440f145c
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 17:04:57 +0900

    Feat: 채팅 입출력 기능
    - Footer에서 입력하면 Main에 출력하도록 기능 구성
    - Fix: leeyj라는 컴포넌트는 ESLint가 대문자로 시작하라고 해서 Leeyj로 바꿈

--- src/components/chat/Footer.tsx
+++ src/components/chat/Footer.tsx
@@ -1,14 +1,21 @@
 // 채팅 입력하는 부분이 생길 예정
 import { useState, ChangeEvent, FormEvent } from 'react';
+import type { FC } from 'react';
 
-const Footer = () => {
+interface FooterProps {
+  // TODO: 혜성 엑스퍼트의 HELP. message 변수를 잘 쓰고 있는데 ESLint가 억까하는 중입니다.
+  // eslint-disable-next-line no-unused-vars
+  addChatContents: (message: string) => void;
+}
+
+const Footer: FC<FooterProps> = ({ addChatContents }) => {
   const [message, setMessage] = useState('');
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
     if (message) {
-      console.log(message);
+      addChatContents(message);
       setMessage('');
     }
   };
