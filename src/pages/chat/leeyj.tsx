adacc1d66edc348a07d436e28ed2710454748c82
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 17:04:57 +0900

    Feat: 채팅 입출력 기능
    - Footer에서 입력하면 Main에 출력하도록 기능 구성
    - Fix: leeyj라는 컴포넌트는 ESLint가 대문자로 시작하라고 해서 Leeyj로 바꿈

--- src/pages/chat/leeyj.tsx
+++ src/pages/chat/leeyj.tsx
@@ -2,16 +2,27 @@ import Footer from '@/components/chat/Footer';
 import Header from '@/components/chat/Header';
 import Main from '@/components/chat/Main';
 import { css } from '@emotion/react';
+import { useState } from 'react';
 
-const leeyj = () => (
-  <main css={mainCss}>
-    <Header />
-    <Main />
-    <Footer />
-  </main>
-);
+const Leeyj = () => {
+  const [chatContents, setChatContents] = useState<{id: number, content: string}[]>([]);
+  const [idCounter, setIdCounter] = useState<number>(0);
 
-export default leeyj;
+  const addChatContent = (message: string) => {
+    setChatContents([...chatContents, { id: idCounter, content: message }]);
+    setIdCounter(idCounter + 1);
+  };
+
+  return (
+    <main css={mainCss}>
+      <Header />
+      <Main chatContents={chatContents} />
+      <Footer addChatContents={addChatContent} />
+    </main>
+  );
+};
+
+export default Leeyj;
 
 const mainCss = css`
   display: flex;
