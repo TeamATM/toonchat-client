54ebcc1f03d803cc2219eb67caf6668f68e3fe47
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-15 01:40:51 +0900

    Style: 친밀도 부분 CSS 적용

--- src/components/chat/FriendShip.tsx
+++ src/components/chat/FriendShip.tsx
@@ -10,54 +10,57 @@ interface UserStatusProps {
 const FriendShip:FC<UserStatusProps> = ({ friendShipExp, maxFriendShipExp, friendShipLv }) => (
   <div
     css={css`
-      width: 100%;
       height: 23px;
       text-align: left;
       font-size: 10px;
       color:  #797C7B;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     `}
   >
-    <b
-      css={css`        
-        line-height: 12px;
-      `}
+    <div css={css`
+      display:flex;
+      align-items: center;
+    `}
     >
-      <span>
-        <span>친밀도</span>
-      </span>
+      <div css={css`padding-right: 5px;`}>친밀도</div>
       <span
         css={css`
           font-size: 12px;
           color: #20a090;
         `}
       >
-        <span> </span>
-        <span>
-          Lv.
-          {' '}
-          {friendShipLv}
-        </span>
+        Lv.
+        {' '}
+        {friendShipLv}
       </span>
-    </b>
-    <div
-      css={css`
-        position: absolute;
-        border-top: 4px solid #cdd1d0;
-        width: 60px;
-        height: 4px;
-        opacity: 0.5;
-        border-radius: 5px;
-      `}
-    />
-    <div
-      css={css`
-        position: absolute;
-        border-top: 4px solid #20a090;
-        width: ${(friendShipExp / maxFriendShipExp) * 60}px;
-        height: 4px;
-        border-radius: 5px;
-      `}
-    />
+    </div>
+    <div css={css`
+      display: flex;
+      padding:5px;
+    `}
+    >
+      <div
+        css={css`
+          /* position: absolute; */
+          border-top: 4px solid #cdd1d0;
+          width: ${(1 - (friendShipExp / maxFriendShipExp)) * 60}px;
+          height: 4px;
+          opacity: 0.5;
+          border-radius: 5px;
+        `}
+      />
+      <div
+        css={css`
+          /* position: absolute; */
+          border-top: 4px solid #20a090;
+          width: ${(friendShipExp / maxFriendShipExp) * 60}px;
+          height: 4px;
+          border-radius: 5px;
+        `}
+      />
+    </div>
   </div>
 );
 
