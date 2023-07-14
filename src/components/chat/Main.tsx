a61e589a39799af636de3cfe141ba0738dbc0aea
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-15 02:59:01 +0900

    Style: 내가 말한 채팅 CSS 적용
    - 이제 영준 캐릭터가 말한 CSS 작업을 해야함

--- src/components/chat/Main.tsx
+++ src/components/chat/Main.tsx
@@ -1,23 +1,30 @@
 // 주요 채팅 내용이 들어올 예정
 import type { FC } from 'react';
+import { css } from '@emotion/react';
+import MySpeak from './MySpeak';
 
 interface MainProps {
   chatContents: {id: number, speaker: string, content: string}[];
 }
 
 const Main: FC<MainProps> = ({ chatContents }) => (
-  <main>
+  <main css={mainCSS}>
     {chatContents.map(
       (chat) => (
-        <li key={chat.id}>
-          {chat.speaker}
-          {' '}
-          :
-          {chat.content}
-        </li>
+        <MySpeak key={chat.id} id={chat.id} content={chat.content} />
       ),
     )}
   </main>
 );
 
 export default Main;
+
+const mainCSS = css`
+  padding: 5px;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
