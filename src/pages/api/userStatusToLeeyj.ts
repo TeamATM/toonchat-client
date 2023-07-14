1d064df92e1a3e79306c08ad8029cf7995179753
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 23:38:25 +0900

    Feat: 임시로 사용할 친밀도 API

---
+++ src/pages/api/userStatusToLeeyj.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  friendShipExp: number,
+  maxFriendShipExp: number,
+  friendShipLv: number
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  setTimeout(() => {
+    res.status(200).json({
+      friendShipExp: 100,
+      maxFriendShipExp: 200,
+      friendShipLv: 0,
+    });
+  }, Math.random() * 3000);
+}
