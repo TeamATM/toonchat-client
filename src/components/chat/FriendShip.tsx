a0701fe5c248e6314f254a4842a94dc3769cd984
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 23:21:50 +0900

    Feat: 채팅방 친밀도 컴포넌트 추가
    - 형태적으로는 만들었음
    - UserStatus를 API로 받아오는 방식을 구현해야함

---
+++ src/components/chat/FriendShip.tsx
@@ -0,0 +1,66 @@
+import { css } from '@emotion/react';
+import type { FC } from 'react';
+
+interface UserStatusProps {
+  friendShipExp: number,
+  maxFriendShipExp: number,
+  friendShipLv: number,
+}
+
+const FriendShip:FC<UserStatusProps> = ({ friendShipExp, maxFriendShipExp, friendShipLv }) => (
+  <div
+    css={css`
+      width: 100%;
+      height: 23px;
+      text-align: left;
+      font-size: 10px;
+      color:  #797C7B;
+    `}
+  >
+    <b
+      css={css`        
+        line-height: 12px;
+      `}
+    >
+      <span>
+        <span>친밀도</span>
+      </span>
+      <span
+        css={css`
+          font-size: 12px;
+          color: #20a090;
+        `}
+      >
+        <span> </span>
+        <span>
+          Lv.
+          {' '}
+          {friendShipLv}
+        </span>
+      </span>
+    </b>
+    <div
+      css={css`
+        position: absolute;
+        border-top: 4px solid #cdd1d0;
+        box-sizing: border-box;
+        width: 60px;
+        height: 4px;
+        opacity: 0.5;
+        border-radius: 5px;
+      `}
+    />
+    <div
+      css={css`
+        position: absolute;
+        border-top: 4px solid #20a090;
+        box-sizing: border-box;
+        width: ${(friendShipExp / maxFriendShipExp) * 60}px;
+        height: 4px;
+        border-radius: 5px;
+      `}
+    />
+  </div>
+);
+
+export default FriendShip;
--- src/components/chat/Header.tsx
+++ src/components/chat/Header.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import FriendShip from './FriendShip';
 
-// 이영준의 이미지, 소속, 이름이 있을 예정
+// TODO: Back 버튼을 누르면 지금 홈으로 돌아가지만 채팅 리스트뷰가 완성되면 그쪽으로 Link 될 예정
 const Header = () => (
   <header>
     <Link href="/">
@@ -21,7 +22,18 @@ const Header = () => (
     />
     <div>이영준</div>
     <div>#네이버웹툰 #김비서가왜그럴까</div>
+    <FriendShip
+      friendShipExp={userStatus.friendShipExp}
+      maxFriendShipExp={userStatus.maxFriendShipExp}
+      friendShipLv={userStatus.friendShipLv}
+    />
   </header>
 );
 
 export default Header;
+
+const userStatus = {
+  friendShipExp: 100,
+  maxFriendShipExp: 200,
+  friendShipLv: 0,
+};
