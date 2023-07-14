69319e02b18721a84b3d83040d8240475fe2822d
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 14:45:22 +0900

    Feat: 채팅입력 form 생성
    - 입력을 하면 일단 console.log로 메시지 출력하고 입력한 텍스트 제거
    - 메시지 출력 부분을 채팅방에 올려보자

--- src/components/chat/Footer.tsx
+++ src/components/chat/Footer.tsx
@@ -1,6 +1,30 @@
 // 채팅 입력하는 부분이 생길 예정
-const Footer = () => (
-  <footer>채팅내용 입력하는 곳</footer>
-);
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+const Footer = () => {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (message) {
+      console.log(message);
+      setMessage('');
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <footer>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={message} onChange={handleChange} required />
+        <button type="submit">Send</button>
+      </form>
+    </footer>
+  );
+};
 
 export default Footer;
