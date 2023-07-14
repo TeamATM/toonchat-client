e0528b05ac6f468a3da2429b964797b4635689b5
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-14 22:10:49 +0900

    Feat: 임시 API 호출
    - 간단한 대화가 가능하도록 API 생성하고 이어붙임

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,12 +2,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
+  say: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  setTimeout(() => {
+    res.status(200).json({ say: jySay() });
+  }, Math.random() * 3000);
 }
+
+const jySay = () => {
+  const yjSayList = ['난 이영준. 세상에서 가장 완벽한 남자.', '훗... 나와 대화하고 싶다면 김비서를 통해서 말해줘야겠어.', '(힐끗보고 무시한다) ...'];
+  return yjSayList[Math.floor(Math.random() * 3)];
+};
