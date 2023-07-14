ed732aefa9ce0f0ce0e342d00fd57c963bd942c9
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-15 00:10:34 +0900

    Style: Send 버튼 svg 적용

--- src/components/chat/Footer.tsx
+++ src/components/chat/Footer.tsx
@@ -1,6 +1,8 @@
 // 채팅 입력하는 부분이 생길 예정
 import { useState, ChangeEvent, FormEvent } from 'react';
 import type { FC } from 'react';
+import { css } from '@emotion/react';
+import Image from 'next/image';
 
 interface FooterProps {
   // TODO: 혜성 엑스퍼트의 HELP. message 변수를 잘 쓰고 있는데 ESLint가 억까하는 중입니다.
@@ -25,13 +27,33 @@ const Footer: FC<FooterProps> = ({ addChatContents }) => {
   };
 
   return (
-    <footer>
+    <footer css={footerCSS}>
       <form onSubmit={handleSubmit}>
         <input type="text" value={message} onChange={handleChange} required />
-        <button type="submit">Send</button>
+        <button css={buttonCSS} type="submit">
+          <Image
+            src="/send.svg"
+            alt="send"
+            width={40}
+            height={40}
+            priority
+          />
+        </button>
       </form>
     </footer>
   );
 };
 
 export default Footer;
+
+const footerCSS = css`
+  width: 100%;
+  height: 80px;
+  text-align: left;
+  font-size: 12px;
+  color: #0F0C22;
+`;
+
+const buttonCSS = css`
+  border: none;
+`;
