3b28f4def5ea683138d18173a3baf805779ee719
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-16 00:10:25 +0900

    Feat: 채팅 올리면 timestamp 생성
    - 유저, 캐릭터의 채팅 컴포넌트 넣기 직전까지 timestamp 꽂힌 것을 console.log로 확인

--- src/pages/chat/leeyj.tsx
+++ src/pages/chat/leeyj.tsx
@@ -5,26 +5,33 @@ import { css } from '@emotion/react';
 import { useState } from 'react';
 
 interface chatContentsState {
-  id: number, speaker: string, content: string
+  id: number, speaker: string, content: string, date: number
 }
 
 const Leeyj = () => {
   const [chatContents, setChatContents] = useState<chatContentsState[]>([]);
   const [idCounter, setIdCounter] = useState<number>(0);
 
-  const addChatContent = async (message: string) => {
+  const addChatContent = async (message: string, date: number) => {
     // TODO: AI 대답 API나 소켓 통신이 가능하게 되면 다시 돌려놓을 코드
-    setChatContents([...chatContents, { id: idCounter, speaker: 'me', content: message }]);
+    setChatContents([...chatContents, {
+      id: idCounter, speaker: 'me', content: message, date,
+    }]);
     // setIdCounter(idCounter + 1);
 
     // TODO: AI의 대답으로 수정될 부분 (API 호출했다고 가정)
-    return callLeeyjAPI(message);
+    return callLeeyjAPI(message, date);
   };
 
-  const callLeeyjAPI = async (message : string) => {
+  const callLeeyjAPI = async (message : string, date: number) => {
     const response = await fetch('/api/hello');
     const jsonData = await response.json();
-    setChatContents([...chatContents, { id: idCounter, speaker: 'me', content: message }, { id: idCounter + 1, speaker: '이영준', content: jsonData.say }]);
+    setChatContents([...chatContents, {
+      id: idCounter, speaker: 'me', content: message, date,
+    },
+    {
+      id: idCounter + 1, speaker: '이영준', content: jsonData.say, date: Date.now(),
+    }]);
     setIdCounter(idCounter + 2);
   };
 
