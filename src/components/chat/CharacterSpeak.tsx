85e62ea469e6781dfa44c15b969da8b1c7f124e3
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-15 23:17:46 +0900

    Style: 영준의 채팅 CSS 수정, 이미지 추가
    - 폰트 사이즈, 패딩도 줄임

--- src/components/chat/CharacterSpeak.tsx
+++ src/components/chat/CharacterSpeak.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import Image from 'next/image';
 import { css } from '@emotion/react';
 
 interface CharacterSpeakProps {
@@ -6,10 +7,19 @@ interface CharacterSpeakProps {
 }
 
 const CharacterSpeak: FC<CharacterSpeakProps> = ({ speaker, content }) => (
-  <span css={css`width:100%; margin-top:3px;`}>
-    <span>{speaker}</span>
-    <span css={characterSpeakCSS}>
-      {content}
+  <span css={css`width:100%; margin-top:3px; display:flex; `}>
+    <Image
+      src="/leeyj.png"
+      width={40}
+      height={40}
+      style={imageStyle}
+      alt="이영준"
+    />
+    <span css={css`display: flex; flex-direction: column;`}>
+      <span css={css`font-size:14px; padding:1px;`}>{speaker}</span>
+      <span css={characterSpeakCSS}>
+        {content}
+      </span>
     </span>
   </span>
 );
@@ -18,10 +28,15 @@ export default CharacterSpeak;
 
 const characterSpeakCSS = css`
   float:left;
-  text-align: right;
-  font-size: 15px;
+  text-align: left;
+  font-size: 12px;
   color: #000E08;
   background-color: #F2F7FB;
   border-radius: 0px 10px 10px 10px;
-  padding: 15px;
+  padding: 12px;
 `;
+
+const imageStyle = {
+  borderRadius: '50%',
+  margin: '5px',
+};
