970f26897dc6c2f9dcf6a2ce4616bd610efd815f
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-15 22:55:04 +0900

    Style: 영준이의 채팅 CSS 적용

--- src/components/chat/CharacterSpeak.tsx
+++ src/components/chat/CharacterSpeak.tsx
@@ -1,14 +1,27 @@
 import type { FC } from 'react';
+import { css } from '@emotion/react';
 
 interface CharacterSpeakProps {
   speaker:string, content: string
 }
 
 const CharacterSpeak: FC<CharacterSpeakProps> = ({ speaker, content }) => (
-  <>
-    <div>{speaker}</div>
-    <div>{content}</div>
-  </>
+  <span css={css`width:100%; margin-top:3px;`}>
+    <span>{speaker}</span>
+    <span css={characterSpeakCSS}>
+      {content}
+    </span>
+  </span>
 );
 
 export default CharacterSpeak;
+
+const characterSpeakCSS = css`
+  float:left;
+  text-align: right;
+  font-size: 15px;
+  color: #000E08;
+  background-color: #F2F7FB;
+  border-radius: 0px 10px 10px 10px;
+  padding: 15px;
+`;
