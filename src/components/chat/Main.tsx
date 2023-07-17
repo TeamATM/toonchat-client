7ce3adfa31ba75efbc529a137a9c33b1eb8c2bfd
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-17 22:05:56 +0900

    Feat: 채팅을 작성하면 스크롤 맨 아래로
    - useRef 를 사용하면 js로 html 태그를 select하는 것처럼 DOM 객체에 접근할 수 있게 해주는 Hook
    - useEffect를 통해서 그 채팅이 업데이트 될 때마다 랜더링을 돌린다
    - 랜더링이 될 때 ref로 참조해놓은 맨 아래 부분으로 스크롤 다운

--- src/components/chat/Main.tsx
+++ src/components/chat/Main.tsx
@@ -1,5 +1,5 @@
 // 주요 채팅 내용이 들어올 예정
-import type { FC } from 'react';
+import { useRef, type FC, useEffect } from 'react';
 import { css } from '@emotion/react';
 import MySpeak from './MySpeak';
 import CharacterSpeak from './CharacterSpeak';
@@ -8,25 +8,34 @@ interface MainProps {
   chatContents: {id: number, speaker: string, content: string, timestamp: number}[];
 }
 
-const Main: FC<MainProps> = ({ chatContents }) => (
-  <main css={mainCSS}>
-    {chatContents.map(
-      (chat) => {
-        if (chat.speaker === 'me') {
-          return <MySpeak key={chat.id} content={chat.content} timestamp={chat.timestamp} />;
-        }
-        return (
-          <CharacterSpeak
-            key={chat.id}
-            speaker={chat.speaker}
-            content={chat.content}
-            timestamp={chat.timestamp}
-          />
-        );
-      },
-    )}
-  </main>
-);
+const Main: FC<MainProps> = ({ chatContents }) => {
+  const messageEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [chatContents]);
+
+  return (
+    <main css={mainCSS}>
+      {chatContents.map(
+        (chat) => {
+          if (chat.speaker === 'me') {
+            return <MySpeak key={chat.id} content={chat.content} timestamp={chat.timestamp} />;
+          }
+          return (
+            <CharacterSpeak
+              key={chat.id}
+              speaker={chat.speaker}
+              content={chat.content}
+              timestamp={chat.timestamp}
+            />
+          );
+        },
+      )}
+      <div ref={messageEndRef} />
+    </main>
+  );
+};
 
 export default Main;
 
