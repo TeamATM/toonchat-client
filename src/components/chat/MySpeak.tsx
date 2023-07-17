9840720345e959b0ef32e9f887ef6de38cb96f27
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-17 20:56:01 +0900

    Refactor: color를 enum으로 바꿈

--- src/components/chat/MySpeak.tsx
+++ src/components/chat/MySpeak.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import { css } from '@emotion/react';
+import color from '@/styles/color';
 import TimeStamp from './TimeStamp';
 
 interface MySpeakProps {
@@ -24,7 +25,7 @@ const mySpeakCSS = css`
   text-align: right;
   font-size: 12px;
   color: #fff;
-  background-color: #20A090;
+  background-color: ${color.lightGreen};
   border-radius: 10px 0px 10px 10px;
   padding: 12px;
 `;
