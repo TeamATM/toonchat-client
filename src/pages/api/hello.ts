b202c1593a864d42fb9ac7a771d34a30ed13eba0
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-17 21:26:13 +0900

    Feat: 긴 말을 하도록 API 수정

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -15,6 +15,6 @@ export default function handler(
 }
 
 const jySay = () => {
-  const yjSayList = ['난 이영준. 세상에서 가장 완벽한 남자.', '훗... 나와 대화하고 싶다면 김비서를 통해서 말해줘야겠어.', '(힐끗보고 무시한다) ...'];
-  return yjSayList[Math.floor(Math.random() * 3)];
+  const yjSayList = ['난 이영준. 세상에서 가장 완벽한 남자.', '훗... 나와 대화하고 싶다면 김비서를 통해서 말해줘야겠어.', '(힐끗보고 무시한다) ...', '말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?말을 길게 하면 어뜨케 처리가 되는 지 아나?'];
+  return yjSayList[Math.floor(Math.random() * yjSayList.length)];
 };
