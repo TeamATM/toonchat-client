3b5018afc622762a508fb938576369d74664b4d3
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-18 18:46:11 +0900

    Chore: eslint의 no-unused-vars 이슈
    - https://blog.pumpkin-raccoon.com/79
    - 타입스크립트의 인터페이스와 ESLint가 충돌되는 상황
    -기존 eslint의 no-unused-vars는 설정을 off하고, typescript-eslint의 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,5 +54,7 @@ module.exports = {
         js: 'never', jsx: 'never', ts: 'never', tsx: 'never',
       },
     ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
 };
