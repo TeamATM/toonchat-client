bd4c04a30b9aaaa803e4350b0927c97da4870750
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-21 03:08:54 +0900

    Feat: 채팅API 응답 완료시 사용될 store 함수 추가
    - addChatContents 함수로 채팅을 추가하면 추가한 채팅의 index값을 return하도록 변경함
    - return 받은 index값을 바탕으로 loading이 완료되면 메시지 얹어주는 loadedChat 함수 추가

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,23 +1,39 @@
 import { create } from 'zustand';
 
 interface ChatContentsState {
-  id: number, speaker: string, content: string, timestamp: number
+  id: number, speaker: string, content: string, timestamp: number, loading?: boolean,
 }
 
 interface NewChatContentState {
-  speaker: string, content: string, timestamp: number
+  speaker: string, content: string, timestamp: number, loading?: boolean,
 }
 
 interface ChatState {
   chatContents: ChatContentsState[],
-  addChatContents: (newChat: NewChatContentState) => void;
+  addChatContents: (newChat: NewChatContentState) => number;
+  loadedChat: (loadedIndex: number, message: string, timestamp: number) => void;
 }
 
-const useChatStore = create<ChatState>((set) => ({
+const useChatStore = create<ChatState>((set, get) => ({
   chatContents: [],
-  addChatContents: (newChat) => set(
+  addChatContents: (newChat) => {
+    set(
+      (state) => ({
+        chatContents: [...state.chatContents, { ...newChat, id: state.chatContents.length + 1 }],
+      }),
+    );
+    return get().chatContents.length - 1;
+  },
+  loadedChat: (loadedIndex, message, timestamp) => set(
     (state) => ({
-      chatContents: [...state.chatContents, { ...newChat, id: state.chatContents.length + 1 }],
+      chatContents: state.chatContents.map((chatContent, index) => {
+        if (index === loadedIndex) {
+          return {
+            ...chatContent, loading: false, content: message, timestamp,
+          };
+        }
+        return chatContent;
+      }),
     }),
   ),
 }));
