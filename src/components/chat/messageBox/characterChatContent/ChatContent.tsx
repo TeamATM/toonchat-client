f1e42dfd6bfab66dea90fe8d52e986bdf149c24d
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-21 04:06:33 +0900

    🐝 Fix: 캐릭터 채팅에 CSS 적용이 잘못된 부분 수정
    - 직전 커밋의 리팩토링 과정에서 생긴 characterChatBoxCSS 관련해서 수정함

--- src/components/chat/messageBox/characterChatContent/ChatContent.tsx
+++ src/components/chat/messageBox/characterChatContent/ChatContent.tsx
@@ -1,11 +1,29 @@
 import { FC } from 'react';
+import { css } from '@emotion/react';
+import color from '@/styles/color';
+import TimeStamp from '../TimeStamp';
 
-interface ChatContentProps { content: string }
+interface ChatContentProps {
+  content: string, timestamp: number
+}
 
-const ChatContent: FC<ChatContentProps> = ({ content }) => (
-  <div>
-    {content}
-  </div>
+const ChatContent: FC<ChatContentProps> = ({ content, timestamp }) => (
+  <>
+    <span css={characterChatBoxCSS}>
+      {content}
+    </span>
+    <TimeStamp timestamp={timestamp} />
+  </>
 );
 
 export default ChatContent;
+
+const characterChatBoxCSS = css`
+  float: left;
+  text-align: left;
+  font-size: 12px;
+  color: ${color.black};
+  background-color: #F2F7FB;
+  border-radius: 0px 10px 10px 10px;
+  padding: 12px;
+`;
