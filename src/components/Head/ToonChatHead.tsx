2a56c866a4caec9d9119b20a40edba0959043161
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-20 16:53:39 +0900

    Style: meta tag 오타 수정
    - meta 태그의 og의 property를 name으로 한 참사를 수정함

--- src/components/Head/ToonChatHead.tsx
+++ src/components/Head/ToonChatHead.tsx
@@ -13,12 +13,12 @@ const ToonChatHead = () => (
     <meta name="apple-mobile-web-app-title" content="ToonChat" />
 
     {/* OG */}
-    <meta name="og:site_name" content="ToonChat" />
-    <meta name="og:title" content="ToonChat" />
-    <meta name="og:type" content="website" />
-    <meta name="og:url" content="/thumbnail.png" />
+    <meta property="og:site_name" content="ToonChat" />
+    <meta property="og:title" content="ToonChat" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="/thumbnail.png" />
     <meta property="og:image" content="/thumbnail.png" />
-    <meta name="og:description" content="최애 캐릭터와의 1:1 대화 | ToonChat" />
+    <meta property="og:description" content="최애 캐릭터와의 1:1 대화 | ToonChat" />
 
     {/* 트위터 */}
     <meta name="twitter:card" content="summary" />
