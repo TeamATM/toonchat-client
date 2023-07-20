0b53f04f2c16b243d30ddeb1bf1b142ab57d5755
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-20 16:04:34 +0900

    Refactor: Chat Main에 store 적용
    - store에서 채팅내용을 main으로 바로 받아올 수 있도록 적용
    - page leeyj에는 이제 더 이상 채팅 내용이 존재하지 않아도 됨

--- src/pages/chat/leeyj.tsx
+++ src/pages/chat/leeyj.tsx
@@ -1,20 +1,15 @@
 import Footer from '@/components/chat/Footer';
 import Header from '@/components/chat/Header';
 import Main from '@/components/chat/Main';
-import useChatStore from '@/store/chat';
 import { css } from '@emotion/react';
 
-const Leeyj = () => {
-  const { chatContents } = useChatStore();
-
-  return (
-    <section css={pageCSS}>
-      <Header />
-      <Main chatContents={chatContents} />
-      <Footer />
-    </section>
-  );
-};
+const Leeyj = () => (
+  <section css={pageCSS}>
+    <Header />
+    <Main />
+    <Footer />
+  </section>
+);
 
 export default Leeyj;
 
