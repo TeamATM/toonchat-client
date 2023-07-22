ad040216590a6ef5486c6295b52c164cfb4ebe6a
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-22 17:54:30 +0900

    Feat: 구분선 컴포넌트 생성
    - 구분선 가운데에 텍스트를 넣는 컴포넌트와 텍스트를 넣지 않는 컴포넌트 구현
    - 구분선의 색을 LightGray로 정의

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,6 +1,7 @@
 import Button from '@/components/user/Button';
+import DivideLine from '@/components/user/DivideLine';
+import DivideLineText from '@/components/user/DivideLineText';
 import Input from '@/components/user/Input';
-import color from '@/styles/color';
 import { css } from '@emotion/react';
 import Link from 'next/link';
 
@@ -15,19 +16,17 @@ const login = () => (
         or email to continue us
       </h2>
     </header>
-    <main css={css`width: 100%;`}>
+    <main css={css`width: 100%; padding: 24px;`}>
       {/* main은 본격적인 로그인 절차를 할 수 있도록 하는 부분 */}
       <div>
         <button type="submit">google</button>
         <button type="submit">naver</button>
         <button type="submit">apple</button>
       </div>
-      <div>
-        <div css={divLineCSS} />
-        <div>OR</div>
-        <div css={divLineCSS} />
-      </div>
-      <form css={css`padding: 24px;`}>
+
+      <DivideLineText>OR</DivideLineText>
+
+      <form>
         <Input text="Your email" inputType="email" />
         <Input text="Password" inputType="password" />
         <Button>
@@ -35,11 +34,11 @@ const login = () => (
         </Button>
         <div>Forget password?</div>
 
-        <div css={divLineCSS} />
       </form>
     </main>
-    <footer>
+    <footer css={css`width: 100%; padding: 24px;`}>
       {/* footer는 로그인이 아니라 가입을 하고 싶은 사람이 있다면 이용할 태그 */}
+      <DivideLine />
       <div>You don&apos;t have an acount?</div>
       <Link href="/">
         <Button>
@@ -59,8 +58,4 @@ const pageCSS = css`
   justify-content: center;
   align-items: center;
   padding: 10px;
-`;
-
-const divLineCSS = css`
-  border-top: 1px solid ${color.greenGray};
 `;
