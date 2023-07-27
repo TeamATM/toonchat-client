6ef51ef64ca3bda2588b13a76d91c8680bd4b3f3
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-27 16:59:49 +0900

    Feat: 친밀도를 받아오는 API 하드코딩에서 변수로 변경

---
+++ src/pages/api/userStatus/[character_id].tsx
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+interface NextApiRequestWithId extends NextApiRequest {
+  query: {
+    character_id: string;
+  };
+}
+
+type Data = {
+  friendShipExp: number,
+  maxFriendShipExp: number,
+  friendShipLv: number
+}
+
+export default function handler(
+  req: NextApiRequestWithId,
+  res: NextApiResponse<Data>,
+) {
+  const characterId = req.query.character_id;
+
+  console.log(characterId);
+  setTimeout(() => {
+    res.status(200).json({
+      friendShipExp: Math.random() * 200,
+      maxFriendShipExp: 200,
+      friendShipLv: 0,
+    });
+  }, Math.random() * 3000);
+}
