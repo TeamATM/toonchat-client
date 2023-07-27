30518d2d5b191d4a094b7327659fa0441f2761aa
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-27 16:19:29 +0900

    Feat: 채팅방의 캐릭터 정보를 하드코딩에서 props 변경

--- src/components/chat/Header.tsx
+++ src/components/chat/Header.tsx
@@ -5,11 +5,15 @@ import CharacterInfo from './characterHeader/CharacterInfo';
 
 interface CharacterState {
   characterId: string,
-  characterName: string
+  characterName: string,
+  hashTag: string,
+  imageUrl: string,
 }
 
 // TODO: Back 버튼을 누르면 지금 홈으로 돌아가지만 채팅 리스트뷰가 완성되면 그쪽으로 Link 될 예정
-const Header : FC<CharacterState> = ({ characterId, characterName }) => {
+const Header : FC<CharacterState> = ({
+  characterId, characterName, hashTag, imageUrl,
+}) => {
   // TODO: 친밀도를 API로 받아와야 작업이 가능함!
   const [userStatus, setUserStatus] = useState({
     friendShipExp: 0, maxFriendShipExp: 1, friendShipLv: 0,
@@ -22,7 +26,7 @@ const Header : FC<CharacterState> = ({ characterId, characterName }) => {
 
   return (
     <header css={headerCSS}>
-      <CharacterInfo characterId={characterId} characterName={characterName} />
+      <CharacterInfo imageUrl={imageUrl} characterName={characterName} hashTag={hashTag} />
       <FriendShip
         friendShipExp={userStatus.friendShipExp}
         maxFriendShipExp={userStatus.maxFriendShipExp}
