9ed279afcdb957f72ad8c8c9fa70a9d64736cdef
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-28 19:38:22 +0900

    Feat: input 컴포넌트에 value, onChange 적용
    - input 컴포넌트를 통해서 submit에 텍스트들을 전달할 수 있도록 함

--- src/components/user/Input.tsx
+++ src/components/user/Input.tsx
@@ -1,19 +1,29 @@
 import color from '@/styles/color';
 import { css } from '@emotion/react';
-import { FC } from 'react';
+import {
+  ChangeEvent, Dispatch, FC, SetStateAction,
+} from 'react';
 
 interface InputProps {
   text: string, inputType: 'email' | 'text',
+  value: string, setState: Dispatch<SetStateAction<string>>
 }
 
 // TODO: 회원가입, 로그인시 닉네임 길이 등 제약이 필요함
-const Input: FC<InputProps> = ({ text, inputType }) => (
-  <>
-    <div css={textCSS}>{text}</div>
-    <input type={inputType} css={inputTagCSS} required />
-  </>
-);
+const Input: FC<InputProps> = ({
+  text, inputType, value, setState,
+}) => {
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
 
+  return (
+    <>
+      <div css={textCSS}>{text}</div>
+      <input type={inputType} css={inputTagCSS} value={value} onChange={changeHandler} required />
+    </>
+  );
+};
 export default Input;
 
 const textCSS = css`
