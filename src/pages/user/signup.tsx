4ac9223eca27cea0491f2608865de0a5695d2b2e
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-28 18:28:21 +0900

    Feat: Sign up page 생성
    - 생성하고 로그인 페이지 밑에 회원가입 버튼과 연결

--- src/pages/user/signup.tsx
+++ src/pages/user/signup.tsx
@@ -1,80 +1,10 @@
 import { css } from '@emotion/react';
-import ToonChatHead from '@/components/head/ToonChatHead';
-import PageTitle from '@/components/user/PageTitle';
-import UnderLineText from '@/components/user/pageTitle/UnderLineText';
-import PageDescribe from '@/components/user/PageDescribe';
-import SocialLoginButtons from '@/components/user/SocialLoginButtons';
-import DivideLineText from '@/components/user/DivideLineText';
-import Input from '@/components/user/Input';
-import PasswordInput from '@/components/user/PasswordInput';
-import Button from '@/components/user/Button';
-import { FormEvent, useState } from 'react';
-import { useRouter } from 'next/router';
 
-const Signup = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const router = useRouter();
+const signup = () => (
+  <section css={pageCSS}>Sign up page</section>
+);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const signupRes = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({
-        email, username, password, confirmPassword,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const signupData = await signupRes.json();
-    if ('error' in signupData) {
-      // TODO: 회원가입 실패시 알려주는 UI가 필요함
-      alert('회원가입 실패');
-      return;
-    }
-    // 회원가입 성공!
-    router.push({
-      pathname: '/user/login',
-    });
-  };
-
-  return (
-    <>
-      <ToonChatHead title="Sign up" />
-      <section css={pageCSS}>
-        {/* header : 회원가입 페이지에 대한 설명 */}
-        <header>
-          <PageTitle>
-            <span>Sign up with </span>
-            <UnderLineText>Email</UnderLineText>
-          </PageTitle>
-          <PageDescribe>
-            Get chatting with friends and family today
-            by signing up for our chat app!
-          </PageDescribe>
-        </header>
-        <section css={wrapperCSS}>
-          <SocialLoginButtons />
-          <DivideLineText>OR</DivideLineText>
-          <form onSubmit={handleSubmit}>
-            <Input text="Your email" inputType="email" value={email} setState={setEmail} />
-            <Input text="Your name" inputType="text" value={username} setState={setUsername} />
-            <PasswordInput text="Password" value={password} setState={setPassword} />
-            <PasswordInput text="Confirm Password" value={confirmPassword} setState={setConfirmPassword} />
-            <Button theme="white">
-              Create an account
-            </Button>
-          </form>
-        </section>
-      </section>
-    </>
-  );
-};
-
-export default Signup;
+export default signup;
 
 const pageCSS = css`
   height: 100vh;
@@ -83,9 +13,4 @@ const pageCSS = css`
   justify-content: center;
   align-items: center;
   padding: 10px;
-`;
-
-const wrapperCSS = css`
-  width: 100%;
-  padding: 24px;
 `;
