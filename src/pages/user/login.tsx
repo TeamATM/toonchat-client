a1f52cc69d39e01b8d76221ace9283069b275168
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-28 19:38:22 +0900

    Feat: input 컴포넌트에 value, onChange 적용
    - input 컴포넌트를 통해서 submit에 텍스트들을 전달할 수 있도록 함

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -11,48 +11,57 @@ import ToForgetPassword from '@/components/user/ToForgetPassword';
 import UnderLineText from '@/components/user/pageTitle/UnderLineText';
 import { css } from '@emotion/react';
 import Link from 'next/link';
+import { FormEvent, useState } from 'react';
 
-const login = () => (
-  <section css={pageCSS}>
-    {/* header : 로그인 페이지에 대한 설명 */}
-    <ToonChatHead title="Log in" />
-    <header>
-      <PageTitle>
-        <UnderLineText>Log in</UnderLineText>
-        <span> to ToonChat</span>
-      </PageTitle>
-      <PageDescribe>
-        Welcome back!
-        Sign in using your social account
-        or email to continue us
-      </PageDescribe>
-    </header>
-    {/* main : 로그인 작업 */}
-    <main css={wrapperCSS}>
-      <SocialLoginButtons />
-      <DivideLineText>OR</DivideLineText>
-      <form>
-        <Input text="Your email" inputType="email" />
-        <PasswordInput text="Password" />
-        <Button theme="green">
-          Log in
-        </Button>
-        <ToForgetPassword />
-      </form>
-    </main>
-    {/* footer : 회원가입 작업 */}
-    <footer css={wrapperCSS}>
-      <DivideLine />
-      <Link href="/user/signup">
-        <Button theme="white">
-          Sign up
-        </Button>
-      </Link>
-    </footer>
-  </section>
-);
+const Login = () => {
+  const [email, setEmail] = useState('');
 
-export default login;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`email : ${email}`);
+  };
+  return (
+    <section css={pageCSS}>
+      {/* header : 로그인 페이지에 대한 설명 */}
+      <ToonChatHead title="Log in" />
+      <header>
+        <PageTitle>
+          <UnderLineText>Log in</UnderLineText>
+          <span> to ToonChat</span>
+        </PageTitle>
+        <PageDescribe>
+          Welcome back!
+          Sign in using your social account
+          or email to continue us
+        </PageDescribe>
+      </header>
+      {/* main : 로그인 작업 */}
+      <main css={wrapperCSS}>
+        <SocialLoginButtons />
+        <DivideLineText>OR</DivideLineText>
+        <form onSubmit={handleSubmit}>
+          <Input text="Your email" inputType="email" value={email} setState={setEmail} />
+          <PasswordInput text="Password" />
+          <Button theme="green">
+            Log in
+          </Button>
+          <ToForgetPassword />
+        </form>
+      </main>
+      {/* footer : 회원가입 작업 */}
+      <footer css={wrapperCSS}>
+        <DivideLine />
+        <Link href="/user/signup">
+          <Button theme="white">
+            Sign up
+          </Button>
+        </Link>
+      </footer>
+    </section>
+  );
+};
+
+export default Login;
 
 const pageCSS = css`
   height: 100vh;
--- src/pages/user/signup.tsx
+++ src/pages/user/signup.tsx
@@ -8,40 +8,53 @@ import DivideLineText from '@/components/user/DivideLineText';
 import Input from '@/components/user/Input';
 import PasswordInput from '@/components/user/PasswordInput';
 import Button from '@/components/user/Button';
+import { FormEvent, useState } from 'react';
 
-const signup = () => (
-  <>
-    <ToonChatHead title="Sign up" />
-    <section css={pageCSS}>
-      {/* header : 회원가입 페이지에 대한 설명 */}
-      <header>
-        <PageTitle>
-          <span>Sign up with </span>
-          <UnderLineText>Email</UnderLineText>
-        </PageTitle>
-        <PageDescribe>
-          Get chatting with friends and family today
-          by signing up for our chat app!
-        </PageDescribe>
-      </header>
-      <section css={wrapperCSS}>
-        <SocialLoginButtons />
-        <DivideLineText>OR</DivideLineText>
-        <form>
-          <Input text="Your email" inputType="email" />
-          <Input text="Your name" inputType="text" />
-          <PasswordInput text="Password" />
-          <PasswordInput text="Confirm Password" />
-          <Button theme="white">
-            Create an account
-          </Button>
-        </form>
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`username : ${username} / email : ${email}`);
+  };
+
+  return (
+    <>
+      <ToonChatHead title="Sign up" />
+      <section css={pageCSS}>
+        {/* header : 회원가입 페이지에 대한 설명 */}
+        <header>
+          <PageTitle>
+            <span>Sign up with </span>
+            <UnderLineText>Email</UnderLineText>
+          </PageTitle>
+          <PageDescribe>
+            Get chatting with friends and family today
+            by signing up for our chat app!
+          </PageDescribe>
+        </header>
+        <section css={wrapperCSS}>
+          <SocialLoginButtons />
+          <DivideLineText>OR</DivideLineText>
+          <form onSubmit={handleSubmit}>
+            <Input text="Your email" inputType="email" value={email} setState={setEmail} />
+            <Input text="Your name" inputType="text" value={username} setState={setUsername} />
+            <PasswordInput text="Password" />
+            <PasswordInput text="Confirm Password" />
+            <Button theme="white">
+              Create an account
+            </Button>
+          </form>
+        </section>
       </section>
-    </section>
-  </>
-);
+    </>
+  );
+};
 
-export default signup;
+export default Signup;
 
 const pageCSS = css`
   height: 100vh;
