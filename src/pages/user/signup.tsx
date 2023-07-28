bbf5b84ed86d57652a4b926ca179c8e0303aec73
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-28 18:33:26 +0900

    Feat: 회원가입 페이지 Header, Head 추가

--- src/pages/user/signup.tsx
+++ src/pages/user/signup.tsx
@@ -1,7 +1,26 @@
 import { css } from '@emotion/react';
+import ToonChatHead from '@/components/head/ToonChatHead';
+import PageTitle from '@/components/user/PageTitle';
+import UnderLineText from '@/components/user/pageTitle/UnderLineText';
+import PageDescribe from '@/components/user/PageDescribe';
 
 const signup = () => (
-  <section css={pageCSS}>Sign up page</section>
+  <>
+    <ToonChatHead title="Sign up" />
+    <section css={pageCSS}>
+      {/* header : 회원가입 페이지에 대한 설명 */}
+      <header>
+        <PageTitle>
+          <span>Sign up with </span>
+          <UnderLineText>Email</UnderLineText>
+        </PageTitle>
+        <PageDescribe>
+          Get chatting with friends and family today
+          by signing up for our chat app!
+        </PageDescribe>
+      </header>
+    </section>
+  </>
 );
 
 export default signup;
