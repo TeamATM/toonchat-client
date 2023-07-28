e775761b84f6802905d5ba90930072b76f44c76f
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-28 18:40:38 +0900

    Feat: 회원가입에 필요한 컴포넌트 얹음
    - 확실히 이미 있는 컴포넌트를 재사용할 수 있기에 굉장히 편했음

--- src/pages/user/signup.tsx
+++ src/pages/user/signup.tsx
@@ -3,6 +3,11 @@ import ToonChatHead from '@/components/head/ToonChatHead';
 import PageTitle from '@/components/user/PageTitle';
 import UnderLineText from '@/components/user/pageTitle/UnderLineText';
 import PageDescribe from '@/components/user/PageDescribe';
+import SocialLoginButtons from '@/components/user/SocialLoginButtons';
+import DivideLineText from '@/components/user/DivideLineText';
+import Input from '@/components/user/Input';
+import PasswordInput from '@/components/user/PasswordInput';
+import Button from '@/components/user/Button';
 
 const signup = () => (
   <>
@@ -19,6 +24,19 @@ const signup = () => (
           by signing up for our chat app!
         </PageDescribe>
       </header>
+      <section css={wrapperCSS}>
+        <SocialLoginButtons />
+        <DivideLineText>OR</DivideLineText>
+        <form>
+          <Input text="Your email" inputType="email" />
+          <Input text="Your name" inputType="text" />
+          <PasswordInput text="Password" />
+          <PasswordInput text="Confirm Password" />
+          <Button theme="white">
+            Create an account
+          </Button>
+        </form>
+      </section>
     </section>
   </>
 );
@@ -32,4 +50,9 @@ const pageCSS = css`
   justify-content: center;
   align-items: center;
   padding: 10px;
+`;
+
+const wrapperCSS = css`
+  width: 100%;
+  padding: 24px;
 `;
