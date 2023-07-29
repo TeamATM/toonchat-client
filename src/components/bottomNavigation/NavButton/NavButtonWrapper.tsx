b09f33c561741a530c310a9b4894e079ed7377c8
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-29 16:48:24 +0900

    Style: icon을 image 태그에서 svg 태그로 변경 후 color 적용

--- src/components/bottomNavigation/NavButton/NavButtonWrapper.tsx
+++ src/components/bottomNavigation/NavButton/NavButtonWrapper.tsx
@@ -1,24 +1,40 @@
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 
 interface NavButtonWrapperProps {
   children: ReactNode,
-  linkUrl: string
+  linkUrl: string,
+  color: string,
 }
 
-const NavButtonWrapper: FC<NavButtonWrapperProps> = ({ children, linkUrl }) => (
-  <Link href={linkUrl}>
-    <div css={WrapperCSS}>
-      {children}
-    </div>
+const NavButtonWrapper: FC<NavButtonWrapperProps> = ({ children, linkUrl, color }) => (
+  <Link href={linkUrl} passHref legacyBehavior>
+    <ResetLink>
+      <div css={WrapperCSS(color)}>
+        {children}
+      </div>
+    </ResetLink>
   </Link>
 );
 
 export default NavButtonWrapper;
 
-const WrapperCSS = css`
+const WrapperCSS = (color: string) => css`
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-size: 14px;
+  gap: 4px;
+  color: ${color};
+`;
+
+const ResetLink = styled.a`
+  font-size: 14px;
+  text-decoration: none;
+
+  &:active {
+    text-decoration: none;
+  }
 `;
--- src/components/icons/ProfileIcon.tsx
+++ src/components/icons/ProfileIcon.tsx
@@ -1,13 +1,17 @@
-import Image from 'next/image';
+import { FC } from 'react';
 
-const ProfileIcon = () => (
-  <Image
-    src="/icons/Profile.svg"
-    alt="Profile"
-    width={30}
-    height={30}
-    priority
-  />
+interface IconProps {
+  color: string
+}
+const ProfileIcon: FC<IconProps> = ({ color }) => (
+  <svg width="30" height="30" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21.2926 10.0235C22.0826 10.8136 22.0826 12.0944 21.2926 12.8845C20.5026 13.6745 19.2217 13.6745 18.4316 12.8845C17.6416 12.0944 17.6416 10.8136 18.4316 10.0235C19.2217 9.23349 20.5026 9.23349 21.2926 10.0235" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M14.7026 6.41236C15.9191 7.62884 15.9191 9.60115 14.7026 10.8176C13.4862 12.0341 11.5139 12.0341 10.2974 10.8176C9.08089 9.60116 9.08089 7.62885 10.2974 6.41236C11.5138 5.19588 13.4862 5.19588 14.7026 6.41236" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M6.56846 10.0235C7.35849 10.8136 7.35849 12.0944 6.56846 12.8845C5.77843 13.6745 4.49754 13.6745 3.70751 12.8845C2.91748 12.0944 2.91748 10.8136 3.70751 10.0235C4.49754 9.23349 5.77843 9.23349 6.56846 10.0235" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M23.5 19.5V18.404C23.5 17.023 22.381 15.904 21 15.904H20.199" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M1.5 19.5V18.404C1.5 17.023 2.619 15.904 4 15.904H4.801" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M17.8389 19.5V17.899C17.8389 15.966 16.2719 14.399 14.3389 14.399H10.6599C8.72691 14.399 7.15991 15.966 7.15991 17.899V19.5" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
 );
 
 export default ProfileIcon;
