a1b61037c29a8eddf70920347f9bdf2c286f0cc2
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-31 16:22:31 +0900

    Feat: 채팅내역 페이지 header 부분 구현
    - 기존 컴포넌트를 활용해서 끼움

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,13 +1,21 @@
 import { css } from '@emotion/react';
 import ToonChatHead from '@/components/head/ToonChatHead';
 import BottomNavigation from '@/components/bottomNavigation/BottomNavigation';
+import SearchBar from '@/components/main/SearchBar';
+import SectionTitle from '@/components/main/SectionTitle';
 
 const ChatMain = () => (
   <>
     <ToonChatHead title="Chat" />
     <section css={pageCSS}>
-      채팅내역이 나올 예정인 페이지
-      아직 구현 안됐어용
+      <div css={contentsCSS}>
+        <header css={css`width:100%; padding-top:10px;`}>
+          <div css={titleSectionCSS}>
+            <SectionTitle>Friends</SectionTitle>
+            <SearchBar />
+          </div>
+        </header>
+      </div>
       <BottomNavigation pageName="Chat" />
     </section>
   </>
@@ -22,4 +30,25 @@ const pageCSS = css`
   justify-content: space-between;
   align-items: center;
   padding: 10px;
+`;
+
+const contentsCSS = css`
+  height: 600px;
+  width: 100%;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+`;
+
+const titleSectionCSS = css`
+  width:100%;
+  display:flex;
+  justify-content:space-between;
+  align-items: center;
+  padding-left:20px;
+  padding-right: 10px;
+  padding-top: 10px;
 `;
