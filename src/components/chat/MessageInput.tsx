7897c67d2f633a4fd923beb5266bca1456fe392a
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-31 13:31:21 +0900

    Feat: 임시 API에서 socket 연결로 채팅 이관

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -5,7 +5,6 @@ import {
 import { css } from '@emotion/react';
 import Image from 'next/image';
 import color from '@/styles/color';
-import useChatStore from '@/store/chat';
 import useSocketStore from '@/store/socket';
 import Dialog from '../dialog/Dialog';
 
@@ -14,8 +13,8 @@ interface CharacterState {
   characterName: string
 }
 
-const MessageInput : FC<CharacterState> = ({ characterId, characterName }) => {
-  const { addChatContents, loadedChat } = useChatStore();
+// const MessageInput : FC<CharacterState> = ({ characterId, characterName }) => {
+const MessageInput : FC<CharacterState> = () => {
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
@@ -34,21 +33,11 @@ const MessageInput : FC<CharacterState> = ({ characterId, characterName }) => {
     }
     if (message) {
       setLoading(true);
-      const timestamp = Date.now();
-      addChatContents({
-        speaker: 'me', content: message, timestamp, loading: true,
-      });
-      setMessage('');
-
-      // TODO: AI의 대답으로 수정될 부분 (API 호출했다고 가정)
-      await callLeeyjAPI(timestamp);
-
       /**
-       * AI의 대답으로 수정된 부분
-       * addChatContents (speaker: me)와 callLeeyjAPI 기능을 포함하고 있음
-       */
+       * AI의 대답 호출
+      */
       sendMessage(message);
-
+      setMessage('');
       setLoading(false);
     }
   };
@@ -60,20 +49,6 @@ const MessageInput : FC<CharacterState> = ({ characterId, characterName }) => {
   const closeModal = () => {
     setModalOpen(false);
     inputRef.current?.focus();
-  };
-
-  const callLeeyjAPI = async (timestamp: number) => {
-    const loadingTimestamp = addChatContents({
-      speaker: characterName, content: 'loading', timestamp: Date.now() + new Date('2100-12-31 00:00:00').getTime(), loading: true,
-    });
-
-    const response = await fetch(`/api/chat/${characterId}`, { method: 'POST' });
-    const jsonData = await response.json();
-
-    // 함수의 input값인 message, timestamp를 아직 안쓰고 있어서 콘솔로그 찍어놓음
-    console.log(message, timestamp);
-
-    loadedChat(loadingTimestamp, jsonData.say, Date.now());
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
