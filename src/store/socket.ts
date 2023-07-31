c9a271053d78534817a0defad9ab5a88e46f5d16
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-31 14:00:17 +0900

    Fix: 채팅 응답 부분의 캐릭터 이름 하드코딩
    - 서버에서 언젠가 캐릭터 이름을 같이 보내줄 것이라 믿고 있습니다.
    #32

--- src/store/socket.ts
+++ src/store/socket.ts
@@ -57,13 +57,17 @@ const useSocketStore = create<SocketState>((set, get) => ({
       const characterId = getCurrentCharacterId();
       if (!characterId || message.to !== characterId) return;
 
+      // TODO: 서버에서 소켓을 통해 메시지를 보내줄 때 characterName을 받아와야합니다.
+      // 지금은 임시로 하드코딩을 넣어놓겠습니다.
+      const characterName = characterId === '0' ? '이영준' : '김미소';
+
       // 내 메시지 추가
       get().chatStore?.addChatContents({
         speaker: 'me', content: message.content, timestamp: message.createdAt.getTime(), loading: true,
       });
       // 답으로 올 메시지 추가
       get().chatStore?.addChatContents({
-        speaker: message.to, content: '', timestamp: message.createdAt.getTime() + 1, loading: true,
+        speaker: characterName, content: '', timestamp: message.createdAt.getTime() + 1, loading: true,
       });
       // 나중에 답이 왔을때 해당 메시지를 찾기 위해 메시지 ID 값을 키로 가지는 Map에 정보 저장
       get().processingMessagePool.set(message.replyMessageId, [message.createdAt.getTime() + 1, '']);
@@ -139,7 +143,9 @@ interface MessageObject {
     content:string,
     from:string,
     to:string,
-    createdAt:Date
+    createdAt:Date,
+    // TODO: 소켓으로 보내줄 때 캐릭터 이름도 보내주세용.
+    // characterName:string,
 }
 
 export default useSocketStore;
