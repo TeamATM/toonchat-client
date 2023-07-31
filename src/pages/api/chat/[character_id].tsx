8656735e6c2da6d0a85c7edc54c93b4ffe5bdbc1
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-31 13:31:21 +0900

    Feat: 임시 API에서 socket 연결로 채팅 이관

--- src/pages/api/chat/[character_id].tsx
+++ src/pages/api/chat/[character_id].tsx
@@ -23,28 +23,10 @@ export default function handler(
 ) {
   const characterId = req.query.character_id;
 
-  if (req.method === 'POST') {
-    setTimeout(() => {
-      res.status(200).json({ say: Say(characterId) });
-    }, Math.random() * 5000);
-  } else {
-    setTimeout(() => {
-      res.status(200).json({ history: history(characterId) });
-    }, Math.random() * 5000);
-  }
+  setTimeout(() => {
+    res.status(200).json({ history: history(characterId) });
+  }, Math.random() * 5000);
 }
-
-const Say = (characterId: string) => {
-  if (characterId === '0') {
-    return sayList[0][Math.floor(Math.random() * sayList[0].length)];
-  }
-  return sayList[1][Math.floor(Math.random() * sayList[1].length)];
-};
-
-const sayList = {
-  0: ['난 이영준. 세상에서 가장 완벽한 남자.', '훗... 나와 대화하고 싶다면 김비서를 통해서 말해줘야겠어.', '(힐끗보고 무시한다) ...'],
-  1: ['저는 김미소입니다.', '업무 중이라서 나중에 연락드릴게요!', '(퇴사 생각을 하느라 대답하지 못했습니다.)'],
-};
 
 const history = (characterId: string) => {
   if (characterId === '0') {
