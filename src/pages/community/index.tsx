e8aabf3e7cbe7adf92618b60728350af7d3d7c46
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-07-31 17:59:57 +0900

    Style: 커뮤니티, 프로필 페이지 생성

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,5 +1,24 @@
+import BottomNavigation from '@/components/bottomNavigation/BottomNavigation';
+import ToonChatHead from '@/components/head/ToonChatHead';
+import { css } from '@emotion/react';
+
 const Community = () => (
-  <div>index</div>
+  <>
+    <ToonChatHead title="Community" />
+    <section css={pageCSS}>
+      커뮤니티 기능은 추후 제공될 예정입니다.
+    </section>
+    <BottomNavigation pageName="Community" />
+  </>
 );
 
 export default Community;
+
+const pageCSS = css`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,24 @@
+import BottomNavigation from '@/components/bottomNavigation/BottomNavigation';
+import ToonChatHead from '@/components/head/ToonChatHead';
+import { css } from '@emotion/react';
+
 const Profile = () => (
-  <div>index</div>
+  <>
+    <ToonChatHead title="Profile" />
+    <section css={pageCSS}>
+      프로필 기능은 추후 제공될 예정입니다.
+    </section>
+    <BottomNavigation pageName="Profile" />
+  </>
 );
 
 export default Profile;
+
+const pageCSS = css`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`;
