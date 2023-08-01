0be6d458bcc8d353b2d38f41b6cc62a10bb10cf3
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-01 16:11:26 +0900

    Refactor: /chat을 /chats로 변경

--- src/components/user/ToForgetPassword.tsx
+++ src/components/user/ToForgetPassword.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const ToForgetPassword = () => (
   // TODO: 비밀번호 찾기 페이지 구현하고 라우팅해야함
   <div css={textCSS}>
-    <Link href="/chat/0" passHref legacyBehavior>
+    <Link href="/chats/0" passHref legacyBehavior>
       <RedLink>
         {/* Forget password? */}
         바로 영준이랑 대화하기
