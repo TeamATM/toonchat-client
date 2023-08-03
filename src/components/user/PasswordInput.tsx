3ba1ce5ac739bfd5daa2b8b58ff350560ac95a7e
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-03 14:31:42 +0900

    Style: 자주 쓰는 버튼, 모달, 토스트등 컴포넌트 px에서 rem

--- src/components/user/PasswordInput.tsx
+++ src/components/user/PasswordInput.tsx
@@ -31,17 +31,17 @@ const PasswordInput: FC<PasswordInputProps> = ({ text, value, setState }) => {
 export default PasswordInput;
 
 const textCSS = css`
-  font-size: 14px;
+  font-size: 0.875rem;
   color: ${color.lightGreen};
 `;
 
 const inputTagCSS = css`
   display: block;
   width: 100%;
-  margin-top: 14px;
-  margin-bottom: 15px;
-  padding: 5px;
-  font-size: 16px;
+  margin-top: 0.875rem;
+  margin-bottom: 0.875rem;
+  padding: 0.25rem;
+  font-size: 1rem;
   outline: none;
   background: none;
   border: none;
