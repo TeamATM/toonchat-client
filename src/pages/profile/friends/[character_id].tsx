7bc404508d8037644bb2f34372ae0abb6aafffcb
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-06 23:08:02 +0900

    Feat: 프로필 페이지에 캐릭터 정보 가져오기

--- src/pages/profile/friends/[character_id].tsx
+++ src/pages/profile/friends/[character_id].tsx
@@ -1,5 +1,79 @@
-const FriendProfile = () => (
-  <div>[character_id]</div>
-);
+import { GetServerSideProps } from 'next';
+import ToonChatHead from '@/components/head/ToonChatHead';
+
+interface CharacterProfileProps {
+  characterName: string,
+  characterId: string,
+  hashTag: string,
+  imageUrl: string,
+  backgroundImageUrl: string,
+  statusMessage: string,
+}
+
+const FriendProfile = ({
+  characterProfileProps: {
+    characterName, characterId, hashTag, imageUrl, backgroundImageUrl, statusMessage,
+  },
+} : {characterProfileProps: CharacterProfileProps }) => {
+  console.log(characterName, characterId, hashTag, imageUrl, backgroundImageUrl, statusMessage);
+
+  return (
+    <>
+      <ToonChatHead title={`${characterName}의 프로필`} />
+      <div>{`캐릭터 이름 : ${characterName}`}</div>
+      <div>{`Hash Tag : ${hashTag}`}</div>
+      <div>{`상태 메시지 : ${statusMessage}`}</div>
+    </>
+  );
+};
 
 export default FriendProfile;
+
+const characterProfileDataSet = [
+  {
+    'bot-name': '이영준',
+    'hash-tag': '#카카오페이지 #김비서가왜그럴까',
+    'image-url': '/leeyj.png',
+    'background-image-url': '/leeyj.png',
+    'status-message': '난 왜 이렇게 완벽한걸까...',
+  }, {
+    'bot-name': '김미소',
+    'hash-tag': '#카카오페이지 #김비서가왜그럴까',
+    'image-url': '/kimms.png',
+    'background-image-url': '/kimms.png',
+    'status-message': '퇴사할 예정입니다. :)',
+  },
+];
+
+export const getServerSideProps
+: GetServerSideProps<{characterProfileProps: CharacterProfileProps}> = async (context) => {
+  const characterId = context.query.character_id;
+  // TODO: 서버에서 캐릭터 profile을 얻게되면 API 호출을 할 예정
+  if (Array.isArray(characterId) || !characterId) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const idNumber = parseInt(characterId, 10);
+  if (Number.isNaN(idNumber) || idNumber < 0 || idNumber >= characterProfileDataSet.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const dataSet = characterProfileDataSet[idNumber];
+
+  return {
+    props: {
+      characterProfileProps: {
+        characterName: dataSet['bot-name'],
+        characterId,
+        hashTag: dataSet['hash-tag'],
+        imageUrl: dataSet['image-url'],
+        backgroundImageUrl: dataSet['background-image-url'],
+        statusMessage: dataSet['status-message'],
+      },
+    },
+  };
+};
