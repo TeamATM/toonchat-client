4951a84792a4db2ac8a0770c3872c9eb1d7af22a
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-10 17:58:22 +0900

    Refactor: Link 초기화하는 반복된 부분을 컴포넌트화

--- src/components/user/ToForgetPassword.tsx
+++ src/components/user/ToForgetPassword.tsx
@@ -1,36 +1,23 @@
 import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-import Link from 'next/link';
 import color from '@/styles/color';
+import LinkWrapper from '@/components/common/link/LinkWrapper';
 
 const ToForgetPassword = () => (
   // TODO: 비밀번호 찾기 페이지 구현하고 라우팅해야함
   <div css={textCSS}>
-    <Link href="/chats/0" passHref legacyBehavior>
-      <RedLink>
-        {/* Forget password? */}
-        바로 영준이랑 대화하기
-      </RedLink>
-    </Link>
+    <LinkWrapper linkUrl="/chats/0">
+      {/* Forget password? */}
+      바로 영준이랑 대화하기
+    </LinkWrapper>
   </div>
 );
 
 export default ToForgetPassword;
-
-const RedLink = styled.a`
-  color: ${color.darkGreen};
-  font-size: 1rem;
-  text-decoration: none;
-
-  &:active {
-    color: ${color.lightGreen};
-    text-decoration: none;
-  }
-`;
 
 const textCSS = css`
   margin: auto;
   text-align: center;
   font-size: 1rem;
   padding: 0.5rem;
+  color: ${color.darkGreen};
 `;
