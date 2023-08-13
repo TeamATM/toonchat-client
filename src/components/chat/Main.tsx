3ad9011934d37b30962a7918a4e9c95fbb124440
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-13 22:02:42 +0900

    Feat: History 가져오는 API 적용
    - 객체 형 변환이 필요해서 utils 폴더 내에 service 폴더를 둠 (service 레이어)

--- src/components/chat/Main.tsx
+++ src/components/chat/Main.tsx
@@ -4,6 +4,7 @@ import {
 } from 'react';
 import { css } from '@emotion/react';
 import useChatStore from '@/store/chat';
+import getHistory from '@/utils/services/chats';
 import MySpeak from './messageBox/MySpeak';
 import CharacterSpeak from './messageBox/CharacterSpeak';
 import Loading from '../common/dialog/Loading';
@@ -14,19 +15,6 @@ interface MainProps {
   imageUrl: string
 }
 
-type BotChat = { bot: string, timestamp: number }
-type HumanChat = { human: string, timestamp: number }
-
-// TODO: util같은 폴더에 따로 모아둬야할 것 같은 기능.
-const historyToContents = (histories: (
-  BotChat|HumanChat)[], characterName: string) => histories.map((history, index) => ({
-  id: index,
-  speaker: 'bot' in history ? characterName : 'me',
-  content: 'bot' in history ? history.bot : history.human,
-  timestamp: history.timestamp,
-  loading: false,
-}));
-
 const Main:FC<MainProps> = ({ characterId, characterName, imageUrl }) => {
   const { chatContents, clearChatContents, initChatContents } = useChatStore();
   const messageEndRef = useRef<HTMLDivElement | null>(null);
@@ -36,13 +24,7 @@ const Main:FC<MainProps> = ({ characterId, characterName, imageUrl }) => {
     // TODO: Loading으로 채팅을 못치게 막아야할 것 같음
     clearChatContents();
     setLoadingHistory(true);
-    fetch(`/api/chat/${characterId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setLoadingHistory(false);
-        initChatContents(historyToContents(data.history, characterName));
-      });
-    // setLoadingHistory(false);
+    getHistory(setLoadingHistory, characterId, characterName, initChatContents);
   }, [characterId, characterName, clearChatContents, initChatContents]);
   useEffect(() => {
     messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
--- src/utils/api/chats.ts
+++ src/utils/api/chats.ts
@@ -1,15 +1,11 @@
 import defaultInstance from '@/utils/axiosInstance/defaultInstance';
 
-interface RecentData {
-  createdAt: number;
-  content: string;
-  characterName: string;
-}
-
 export const recentChatAPI = async (characterId: string) => {
-  const result = await defaultInstance.get<RecentData>(`${process.env.SERVER_URL || 'http://localhost:8080'}/chat/${characterId}?recent=true`);
+  const result = await defaultInstance.get(`${process.env.SERVER_URL || 'http://localhost:8080'}/chat/${characterId}?recent=true`);
   return result.data;
 };
 
-// TODO: 다른 API 만들기
-export const otherAPI = () => console.log(123);
+export const chatHistoryAPI = async (characterId: string) => {
+  const result = await defaultInstance.get(`${process.env.SERVER_URL || 'http://localhost:8080'}/chat/${characterId}/history`);
+  return result.data;
+};
