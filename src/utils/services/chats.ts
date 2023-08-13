db613f83a58f6b0225a1f9711b72bcf707b4fa7a
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-13 22:02:42 +0900

    Feat: History 가져오는 API 적용
    - 객체 형 변환이 필요해서 utils 폴더 내에 service 폴더를 둠 (service 레이어)

---
+++ src/utils/services/chats.ts
@@ -0,0 +1,32 @@
+import { chatHistoryAPI } from '../api/chats';
+
+interface ChatContentsState {
+  id: number, speaker: string, content: string, timestamp: number, loading: boolean,
+}
+
+interface resultData {
+  id: string, content: string, createdAt: number, messageTo: string;
+}
+
+type GetHistory = (
+  setLoading: (isLoading: boolean) => void,
+  characterId: string,
+  characterName: string,
+  initChatContents: (history:ChatContentsState[]) => void
+) => void
+
+const getHistory: GetHistory = async (setLoading, characterId, characterName, initChatContents) => {
+  setLoading(false);
+  const result = await chatHistoryAPI(characterId);
+  const history = result.map((data: resultData) => ({
+    id: data.id,
+    content: data.content,
+    timestamp: data.createdAt,
+    speaker: data.messageTo === characterId ? 'me' : characterName,
+  }));
+  initChatContents(history);
+};
+
+// TODO: history 관련 service 레이어 함수가 더 많아지면 아래 eslint 막은 것 풀기
+// eslint-disable-next-line import/no-anonymous-default-export
+export default getHistory;
