5dae91aac40e7efcea5323cfc0558b8b8b67cb4a
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-08-14 12:46:54 +0900

    Feat: Axios에 error 핸들링하는 미들웨어 추가
    - 더 커스텀하면 에러 핸들링이 정확하게 되도록 할 수 있을 것 같음

--- src/utils/axiosInstance/defaultInstance.ts
+++ src/utils/axiosInstance/defaultInstance.ts
@@ -1,7 +1,14 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const defaultInstance = axios.create({
   baseURL: process.env.SERVER_URL || 'http://localhost:8080/',
+});
+
+defaultInstance.interceptors.response.use((response) => response, (error: AxiosError) => {
+  if (error.response && error.response.status === 401) {
+    console.error('ERROR');
+  }
+  return Promise.reject(error);
 });
 
 export default defaultInstance;
