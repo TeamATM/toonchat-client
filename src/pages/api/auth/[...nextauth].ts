b05be008c59f6286f6a8391420a39cef4cee6bb6
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-09-27 12:00:53 +0900

    Feat: Next Auth 설정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,38 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+/* eslint-disable no-param-reassign */
+import NextAuth, { NextAuthOptions, Session } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import GoogleProvider from 'next-auth/providers/google';
+import NaverProvider from 'next-auth/providers/naver';
+import KakaoProvider from 'next-auth/providers/kakao';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { credentialsLoginAPI } from '@/utils/api/clientServer';
 
-export const authOptions : NextAuthOptions = {
+interface CustomSession extends Session {
+  accessToken?: string;
+  refreshToken?: string;
+}
+
+interface SessionCallback {
+  session: CustomSession; token: JWT
+}
+
+export const authOptions: NextAuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID as string,
+      clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+    }),
+
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+    }),
+
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
@@ -25,6 +54,45 @@ export const authOptions : NextAuthOptions = {
   ],
   pages: {
     signIn: '/login',
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.type === 'credentials') return true;
+      const response = await fetch('http://localhost:3000/api/users/social-login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: user.email,
+          provider: account?.provider,
+        }),
+      });
+      const data = await response.json();
+      if (data?.accessToken && data?.refreshToken) {
+        user.accessToken = data.accessToken;
+        user.refreshToken = data.refreshToken;
+        return true;
+      }
+
+      return false;
+    },
+
+    async jwt({ token, user }) {
+      if (user?.accessToken) {
+        token.accessToken = user.accessToken;
+      }
+      if (user?.refreshToken) {
+        token.refreshToken = user.refreshToken;
+      }
+      return token;
+    },
+
+    async session({ session, token }: SessionCallback) {
+      (session as CustomSession).accessToken = token.accessToken as string | undefined;
+      (session as CustomSession).refreshToken = token.refreshToken as string | undefined;
+      return session;
+    },
   },
 };
 
---
+++ types/next-auth.d.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface User {
+    accessToken: string;
+    refreshToken: string;
+  }
+}
