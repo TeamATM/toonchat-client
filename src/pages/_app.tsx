0647f7bea8ea33d8a2b130f2f699b0fcab9b8490
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-09-27 12:00:53 +0900

    Feat: Next Auth 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from '@/styles/GlobalStyles';
+import { SessionProvider } from 'next-auth/react';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <GlobalStyles />
-    <Component {...pageProps} />
+    <SessionProvider>
+      <Component {...pageProps} />
+    </SessionProvider>
   </>
 );
 
