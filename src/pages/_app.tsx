2346e0c5739cc8771528d58453c11f6447076ca4
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-09-29 17:40:54 +0900

    Feat: GA tag 주입

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import GlobalStyles from '@/styles/GlobalStyles';
 import { SessionProvider } from 'next-auth/react';
+import GA from '@/components/common/head/GA';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <GlobalStyles />
+    <GA />
     <SessionProvider>
       <Component {...pageProps} />
     </SessionProvider>
