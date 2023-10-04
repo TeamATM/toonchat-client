02175240633a786a7a6930a5ce3f90dd4f0f186f
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-04 17:40:16 +0900

    Feat: history api 연결

--- src/utils/api/chats.ts
+++ src/utils/api/chats.ts
@@ -9,3 +9,18 @@ export const chatHistoryAPI = async (characterId: string) => {
   const result = await defaultInstance.get(`/chat/${characterId}/history`);
   return result.data;
 };
+
+export const chatHistory = async (accessToken: string) => {
+  console.log(accessToken);
+  const token = process.env.EXAMPLE_TOKEN;
+  const header:Headers = new Headers();
+  header.set('Authorization', `Bearer ${token}`);
+  console.log(header);
+
+  const result = await fetch(
+    'https://chat.webtoonchat.com/chat/history/2',
+    { headers: header },
+  );
+  // console.log('result');
+  return result.json();
+};
