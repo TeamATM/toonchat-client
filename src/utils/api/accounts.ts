3998eced6a8b85202977dbce2f917d2c73e7b231
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-05 19:14:56 +0900

    Feat: 소셜로그인 API 연결

--- src/utils/api/accounts.ts
+++ src/utils/api/accounts.ts
@@ -16,8 +16,20 @@ interface SignupForm {
   email: string, name: string, password: string,
 }
 
+interface SocialLoginForm {
+  email: string,
+  password: null,
+  provider: string,
+  name: string
+}
+
 export const credentialsLoginAPI = async (credentials : Credentials) => {
   const result = await defaultInstance.post('members/login', JSON.stringify(credentials));
+  return result.data;
+};
+
+export const socialLoginAPI = async (socialLoginForm : SocialLoginForm) => {
+  const result = await defaultInstance.post('members/social-login', JSON.stringify(socialLoginForm));
   return result.data;
 };
 
