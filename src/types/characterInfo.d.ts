12174deae2f642770fb0593ee37b2c758b35f8e5
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-12 20:39:51 +0900

    Refactor: characterInfo 타입 선언

---
+++ src/types/characterInfo.d.ts
@@ -0,0 +1,9 @@
+export interface CharacterInfo {
+  backgroundUrl: string,
+  characterId: string,
+  hashtags: string,
+  id: number,
+  name: string,
+  profileUrl: string,
+  stateMessage: string
+}
