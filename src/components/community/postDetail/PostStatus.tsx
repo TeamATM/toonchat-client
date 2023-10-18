4fcda0b77dc8d2cf75277964eb63456b6d43d439
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-18 17:04:18 +0900

    Feat: 게시글 status (댓글, 좋아요, 신고 아이콘 추가)

---
+++ src/components/community/postDetail/PostStatus.tsx
@@ -0,0 +1,45 @@
+import { css } from '@emotion/react';
+import HeartIcon from '@/components/icons/HeartIcon';
+import CommentIcon from '@/components/icons/CommentIcon';
+import ReportIcon from '@/components/icons/ReportIcon';
+import color from '@/styles/color';
+
+const PostStatus = () => (
+  <ul css={ulCSS}>
+    <li title="좋아요" css={liCSS}>
+      <HeartIcon color={color.black} />
+      <div css={statusCSS}>1</div>
+    </li>
+    <li title="댓글" css={liCSS}>
+      <CommentIcon color={color.black} />
+      <div css={statusCSS}>3</div>
+    </li>
+    <li title="신고" css={liCSS}>
+      <ReportIcon color={color.black} />
+      <div css={statusCSS}>신고</div>
+    </li>
+  </ul>
+);
+
+export default PostStatus;
+
+const ulCSS = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.25rem;
+  padding-top: 1rem;
+`;
+
+const liCSS = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+const statusCSS = css`
+  font-size: 0.75rem;
+  font-weight: bold;
+  color: ${color.black};
+`;
