ad54a3f6ec3266003cc484ae9b44d7de69e8b400
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-19 21:28:04 +0900

    Feat: 게시글 작성 API 호출 함수 생성

--- src/utils/api/boards.ts
+++ src/utils/api/boards.ts
@@ -9,3 +9,8 @@ export const findPostById = async (characterId: string, postId: string) => {
   const result = await webServerInstance.get(`boards/${characterId}/${postId}`);
   return result.data;
 };
+
+export const createPost = async (characterId: string, title: string, content: string) => {
+  const result = await webServerInstance.post(`boards/${characterId}`, { title, content });
+  return result;
+};
