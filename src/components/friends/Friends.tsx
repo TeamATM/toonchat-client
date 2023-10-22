493da722b6a190073ee7cf5c370b4d07dbb972e5
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-22 14:32:38 +0900

    Refactor: 친구창에서 캐릭터 info 수정

--- src/components/friends/Friends.tsx
+++ src/components/friends/Friends.tsx
@@ -1,27 +1,44 @@
 import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import { CharacterInfo } from '@/types/characterInfo';
+import { findAllCharacters } from '@/utils/api/character';
 import FriendWrapper from './friend/FriendWrapper';
 import FriendHashTag from './friend/FriendHashTag';
 import FriendInfo from './friend/FriendInfo';
 
-const Friends = () => (
-  <section css={friendsWrapperCSS}>
-    {characterDataSet.map((data, index) => (
-      // TODO: 여러 캐릭터가 있을 때 스크롤이 가능한지 확인하기 위함
-      <FriendWrapper
-        // eslint-disable-next-line react/no-array-index-key
-        key={index}
-        linkUrl={`/profile/friends/${data.characterId}`}
-      >
-        <FriendInfo
-          characterName={data.characterName}
-          message={data.statusMessage}
-          imageUrl={data.imageUrl}
-        />
-        <FriendHashTag hashTag={data.hashTag} />
-      </FriendWrapper>
-    ))}
-  </section>
-);
+const Friends = () => {
+  const [characterInfoList, setCharacterInfoList] = useState<CharacterInfo[]>([]);
+
+  useEffect(() => {
+    findAllCharacters()
+      .then((data) => {
+        setCharacterInfoList(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching post:', error);
+      });
+  }, []);
+
+  return (
+    <section css={friendsWrapperCSS}>
+      {characterInfoList.map((characterInfo, index) => (
+        // TODO: 여러 캐릭터가 있을 때 스크롤이 가능한지 확인하기 위함
+        <FriendWrapper
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          linkUrl={`/profile/friends/${characterInfo.characterId}`}
+        >
+          <FriendInfo
+            characterName={characterInfo.characterName}
+            message={characterInfo.statusMessage}
+            imageUrl={characterInfo.profileImageUrl}
+          />
+          <FriendHashTag hashTag={characterInfo.hashTag} />
+        </FriendWrapper>
+      ))}
+    </section>
+  );
+};
 
 export default Friends;
 
@@ -31,21 +48,3 @@ const friendsWrapperCSS = css`
   word-break: keep-all;
   padding: 0.375rem;
 `;
-
-// TODO: 이 부분은 API에서 떼와야하는 부분
-const characterDataSet = [
-  {
-    characterName: '이영준',
-    characterId: '0',
-    hashTag: '#카카오페이지 #김비서가왜그럴까',
-    statusMessage: '난 왜 이렇게 완벽한걸까...',
-    imageUrl: '/leeyj.png',
-
-  }, {
-    characterName: '김미소',
-    characterId: '1',
-    hashTag: '#카카오페이지 #김비서가왜그럴까',
-    statusMessage: '조만간 퇴사하려구요 :)',
-    imageUrl: '/kimms.png',
-  },
-];
