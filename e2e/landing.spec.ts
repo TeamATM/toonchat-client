c13c6066d791f847c01a21e9af68a693e1c56fb5
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-22 16:19:02 +0900

    Test: 지금은 영준과 대화하기를 안 쓸 예정이라 제거

--- e2e/landing.spec.ts
+++ e2e/landing.spec.ts
@@ -15,13 +15,3 @@ test('로그인 페이지 클릭', async ({ page }) => {
   await expect(page).toHaveTitle(/Log in/);
   await expect(page).toHaveURL(/.*login/);
 });
-
-test('영준과 대화하기 클릭', async ({ page }) => {
-  await page.goto('/');
-
-  await page.getByRole('link', { name: '영준이와 대화하기' }).click();
-
-  await expect(page).toHaveTitle(/ToonChat/);
-  await expect(page).toHaveTitle(/대화 with/);
-  await expect(page).toHaveURL(/.*chats\/\d+/);
-});
