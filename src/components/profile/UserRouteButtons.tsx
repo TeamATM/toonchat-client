3e22948cb8f7d8fd81c8b7b46cb87b1a71c2b4fe
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-23 23:46:50 +0900

    Feat: 개인 프로필 수정은 아직 제공되지 않으므로 막아놓음

--- src/components/profile/UserRouteButtons.tsx
+++ src/components/profile/UserRouteButtons.tsx
@@ -1,10 +1,14 @@
 import { css } from '@emotion/react';
 import color from '@/styles/color';
 import { signOut } from 'next-auth/react';
+import Toast from '@/components/common/toast/Toast';
+import { useState } from 'react';
 import LogOutIcon from '../icons/LogoutIcon';
 import ProfileSettingIcon from '../icons/ProfileSettingIcon';
 
 const ProfileRouteButtons = () => {
+  const [toastMessage, setToastMessage] = useState('');
+
   // TODO: 미들웨어로 로그인 여부를 컨트롤할 예정
   // TODO: 백엔드 로그아웃 로직이 변경되고 있는 중이라 아직 쓸 수 없는 코드가 있음
   // const { data: session }: any = useSession();
@@ -14,20 +18,31 @@ const ProfileRouteButtons = () => {
   };
 
   const profileEditHandler = () => {
-    console.log(1);
+    setToastMessage('개인 프로필 수정은 추후에 제공될 예정입니다. :)');
+  };
+
+  const handleToastClose = () => {
+    setToastMessage('');
   };
 
   return (
-    <div css={ButtonsWrapperCSS}>
-      <button onClick={profileEditHandler} css={buttonCSS} type="button">
-        <ProfileSettingIcon color={color.black} />
-        Profile Edit
-      </button>
-      <button onClick={signOutHandler} css={buttonCSS} type="button">
-        <LogOutIcon color={color.black} />
-        Log Out
-      </button>
-    </div>
+    <>
+      <div css={ButtonsWrapperCSS}>
+        <button onClick={profileEditHandler} css={buttonCSS} type="button">
+          <ProfileSettingIcon color={color.black} />
+          Profile Edit
+        </button>
+        <button onClick={signOutHandler} css={buttonCSS} type="button">
+          <LogOutIcon color={color.black} />
+          Log Out
+        </button>
+      </div>
+      {
+        toastMessage
+          ? <Toast message={toastMessage} handleClose={handleToastClose} />
+          : null
+      }
+    </>
   );
 };
 
