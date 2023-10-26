c01f0ab843dc96dfdc0ec4179b14784b0c8fd351
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-26 21:02:03 +0900

    Feat: 추천친구 스켈레톤 적용

--- src/components/friends/Recommends.tsx
+++ src/components/friends/Recommends.tsx
@@ -3,19 +3,28 @@ import { findAllCharacters } from '@/utils/api/character';
 import { CharacterInfo } from '@/types/characterInfo';
 import { useEffect, useState } from 'react';
 import RecommendBox from './recommend/RecommendBox';
+import RecommendBoxSkeleton from '../common/skeleton/RecommendBoxSkeleton';
 
 const Recommends = () => {
   const [characterInfoList, setCharacterInfoList] = useState<CharacterInfo[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     findAllCharacters()
       .then((data) => {
         setCharacterInfoList(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching post:', error);
       });
   }, []);
+
+  if (loading) {
+    return (
+      <RecommendBoxSkeleton />
+    );
+  }
 
   return (
     <div css={recommendsCSS}>
