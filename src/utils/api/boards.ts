0c3580c35997b7a1e7421efa5bf6ae74ef641632
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-10-27 16:54:29 +0900

    Feat: 댓글 API 호출 함수 생성

--- src/utils/api/boards.ts
+++ src/utils/api/boards.ts
@@ -1,3 +1,4 @@
+import { CommentData } from '@/types/post';
 import webServerInstance from '@/utils/axiosInstance/webServerInstance';
 
 export const findBoardById = async (characterId: string) => {
@@ -13,4 +14,19 @@ export const findPostById = async (characterId: string, postId: string) => {
 export const createPost = async (characterId: string, title: string, content: string) => {
   const result = await webServerInstance.post(`boards/${characterId}`, { title, content });
   return result;
+};
+
+export const findCommentsByPostAndCharacterId = async (characterId: string, postId: string) => {
+  const result = await webServerInstance.get<CommentData[]>(`boards/${characterId}/${postId}/comments`);
+  return result.data;
+};
+
+export const createComment = async (characterId: string, postId: string, comment: string) => {
+  const result = await webServerInstance.post(`boards/${characterId}/${postId}/comments`, { comment });
+  return result.data;
+};
+
+export const deleteComment = async (commentId: string) => {
+  const result = await webServerInstance.put(`comments/${commentId}`);
+  return result.data;
 };
