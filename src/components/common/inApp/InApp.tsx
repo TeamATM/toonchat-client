5e6adec8bd94d535e1fd1889170cb08aebc19a0b
소울치킨 <90738604+soulchicken@users.noreply.github.com>
2023-11-03 17:54:28 +0900

    Feat: 메인 페이지에서 user-agent로 분기하여 버튼 생성

--- src/components/common/inApp/InApp.tsx
+++ src/components/common/inApp/InApp.tsx
@@ -8,17 +8,6 @@ const InApp = () => {
     }
   };
 
-  // 인앱 브라우저인지 확인하는 함수
-  const isInAppBrowser = () => {
-    const agents = ['kakao', '[fb', 'instagram', 'trill', 'line'];
-    const userAgent = (typeof window !== 'undefined') ? window.navigator.userAgent.toLowerCase() : '';
-    return agents.some((agent) => userAgent.includes(agent));
-  };
-
-  // 인앱 브라우저가 아니라면 null을 반환하여 컴포넌트를 렌더링하지 않습니다.
-  if (!isInAppBrowser()) {
-    return null;
-  }
   return (
     <button onClick={handleButtonClick} css={ButtonCSS('green')} type="button">
       외부 브라우저에서 열기
